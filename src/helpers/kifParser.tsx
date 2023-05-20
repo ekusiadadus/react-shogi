b22bdb427b7c0929fc4476e5aa19a7b20ed1ce73
ekusiadadus <ekusiadadus@gmail.com>
2023-05-21 01:20:43 +0900

    chroe: :fire: update board

--- src/helpers/kifParser.tsx
+++ src/helpers/kifParser.tsx
@@ -14,13 +14,13 @@ const initialBoard = [
   ],
   [
     null,
-    { type: PieceType.Kaku, direction: "down" },
-    null,
-    null,
-    null,
-    null,
-    null,
     { type: PieceType.Hisha, direction: "down" },
+    null,
+    null,
+    null,
+    null,
+    null,
+    { type: PieceType.Kaku, direction: "down" },
     null
   ],
   [
