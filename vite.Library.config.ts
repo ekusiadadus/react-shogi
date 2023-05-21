f35cd80bfe1a58278a57361974d2815dd9f17791
ekusiadadus <ekusiadadus@gmail.com>
2023-05-21 23:00:21 +0900

    chore: :fire: update package

--- vite.Library.config.ts
+++ vite.Library.config.ts
@@ -1,12 +1,28 @@
-import { resolve } from "path"
 import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import { resolve } from "path"
+import terser from "@rollup/plugin-terser"
 
 export default defineConfig({
+  plugins: [react()],
   build: {
+    minify: false,
     lib: {
       entry: resolve(__dirname, "src/lib/react-shogi/ShogiGame.tsx"),
       name: "ShogiGame",
       fileName: "ShogiGame"
+    },
+    rollupOptions: {
+      // 必要な外部依存関係を指定します。
+      external: ["react", "react-dom"],
+      output: {
+        // ライブラリのグローバル変数名を指定します。
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM"
+        },
+        plugins: [terser()]
+      }
     }
   }
 })
